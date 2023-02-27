cf909c7b63f11403052ed97776796df8fdcc80c2
Amit Rana <Amirana14325@gmail.com>
2023-02-27 15:04:54 +0530

    Added by AMIT RANAcreated student api for with CURD

---
+++ StudentsDetails/Models/department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsDetails.Models
+{
+    public class department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
